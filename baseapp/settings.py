12984c8b871ab8b7ad7569c3952b41a20d778e79
samuel aniekan <samuelaniekan680@gmail.com>
2023-04-23 08:37:03 +0100

    modified

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -171,7 +171,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
