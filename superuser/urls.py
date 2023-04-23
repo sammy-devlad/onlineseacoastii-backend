c72fede66b5d483f9f20fedee8067289b6f66d1e
samuel aniekan <samuelaniekan680@gmail.com>
2023-04-23 08:55:40 +0100

    modified

--- superuser/urls.py
+++ superuser/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="admin-dashboard"),
-    path("users/", views.users_, name="admin-users"),
-    path("users/<int:pk>/", views.user_detail, name="admin-users-details"),
+    path("all-users/", views.users_, name="admin-users"),
+    path("all-users/<int:pk>/", views.user_detail, name="admin-users-details"),
     path("transaction-logs/", views.transactions_, name="admin-transactions"),
     path(
         "transactions/<int:pk>/",
